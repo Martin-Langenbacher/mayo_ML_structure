93a88d1325a4742f2361639fe241159a8ab97a8e
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-03-23 11:39:04 +0100

    first commit

---
+++ src/main/java/mayo_data_model/MayoMlApplication.java
@@ -0,0 +1,13 @@
+package mayo_data_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayoMlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayoMlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mayo_data_model/MayoMlApplicationTests.java
@@ -0,0 +1,13 @@
+package mayo_data_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayoMlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
